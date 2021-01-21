9ea5e742567b3a514ade7bc44f5d5d1368199921
dienoov <alvinodien@yahoo.co.id>
2021-01-21 13:13:05 +0700

    (+) Stream yousup

---
+++ app/src/main/java/com/kue/cubit/MainActivity.java
@@ -0,0 +1,31 @@
+package com.kue.cubit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void streamLur(View view) {
+        Intent i = new Intent(MainActivity.this, Stream.class);
+        startActivity(i);
+    }
+
+    public void masukin(View view) {
+        Intent i = new Intent(MainActivity.this, Login.class);
+        startActivity(i);
+    }
+
+    public void singUp(View view) {
+        Intent i = new Intent(MainActivity.this, Login.class);
+        startActivity(i);
+    }
+}
