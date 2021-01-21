e84a71a1885540188565a546dc9ac10ea60c1c03
dienoov <alvinodien@yahoo.co.id>
2021-01-21 13:42:19 +0700

    (+) Login

---
+++ app/src/main/java/com/kue/cubit/LoginTampol.java
@@ -0,0 +1,14 @@
+package com.kue.cubit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginTampol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_tampol);
+    }
+}
